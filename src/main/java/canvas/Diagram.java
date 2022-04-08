87c68a6da977cf9543a4d9d9acbb1a0b455d3ed8
Michele Salvador <michelesalvador@iol.it>
2022-04-08 16:59:50 +0200

    Version 2.1

--- src/main/java/canvas/Diagram.java
+++ src/main/java/canvas/Diagram.java
@@ -1,19 +1,22 @@
 package canvas;
 
+import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Toolkit;
+import java.awt.Stroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.CubicCurve2D;
 import java.io.File;
+import java.util.List;
 import java.util.Set;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -28,7 +31,6 @@ import javax.swing.Timer;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import org.apache.commons.io.FileUtils;
-import org.folg.gedcom.model.EventFact;
 import org.folg.gedcom.model.Gedcom;
 import org.folg.gedcom.model.Person;
 import org.folg.gedcom.parser.JsonParser;
@@ -51,24 +53,27 @@ public class Diagram {
 	JScrollPane scrollPane;
 	Box box;
 	Component lines;
+	Component backLines; // Dashed lines of multi partners
 	static int shiftX = 0;
 	static int shiftY = 0;
 	private Timer timer;
 	float scale = 1;
+	ComponentOrientation orient = ComponentOrientation.RIGHT_TO_LEFT; // LEFT_TO_RIGHT;
 
 	Diagram() throws Exception {
 
-		// Redefine some spacing constants
+		/* Redefine some spacing constants
 		HORIZONTAL_SPACE = 10;
 		BOND_WIDTH = 15;
 		MINI_BOND_WIDTH = 10;
 		MARRIAGE_WIDTH = 30;
 		MARRIAGE_HEIGHT = 20;
+		*/
 
 		// Swing stuff
 		JFrame frame = new JFrame();
-		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
-		//frame.setSize(new Dimension(1700,800));
+		//frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		frame.setSize(new Dimension(1500,700));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// Header
@@ -198,12 +203,12 @@ public class Diagram {
 		graph.initNodes();
 
 		box.setLayout(null); // This non-layout let the nodes in absolute position
-		
+
 		// Add marriage bonds
 		graph.getBonds().forEach(bond -> {
 			box.add(new GraphicBond(bond), 0);
 		});
-		
+
 		if( graph.needMaxBitmap() )
 			graph.setMaxBitmap(3000, 3000); // In Android these values come from canvas.getMaximumBitmapWidth() and canvas.getMaximumBitmapHeight()
 
@@ -211,7 +216,8 @@ public class Diagram {
 		graph.placeNodes();
 
 		// Add the lines
-		lines = box.add(new GraphicLines());
+		lines = box.add(new GraphicLines(graph.getLines(), new BasicStroke(2)));
+		backLines = box.add(new GraphicLines(graph.getBackLines(), new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{3}, 0)));
 
 		// First visible displacement of the diagram
 		displaceDiagram();
@@ -225,11 +231,14 @@ public class Diagram {
 		for( Component compoNode : box.getComponents() ) {
 			if( compoNode instanceof GraphicMetric ) {
 				Metric metric = ((GraphicMetric)compoNode).metric;
-				compoNode.setLocation((int)metric.x + shiftX, (int)metric.y + shiftY);
+				compoNode.setLocation((int)(orient.isLeftToRight() ? metric.x + shiftX
+						: graph.getWidth() - metric.x - metric.width + shiftX), (int)metric.y + shiftY);
 			}
 		}
 		lines.setBounds(shiftX, shiftY, (int)graph.getWidth(), (int)graph.getHeight());
 		lines.repaint();
+		backLines.setBounds(shiftX, shiftY, (int)graph.getWidth(), (int)graph.getHeight());
+		backLines.repaint();
 		box.setPreferredSize(new Dimension(
 				(int)((graph.getWidth() + shiftX * 2) * scale), (int)((graph.getHeight() + shiftY * 2) * scale)));
 		scrollPane.validate(); // Update scrollbars
@@ -260,11 +269,11 @@ public class Diagram {
 			}
 			setBackground(backgroundColor);
 			Color borderColor = Color.gray;
-			if( sex(node.person) == 1 ) {
+			Gender gender = Gender.getGender(node.person);
+			if( gender == Gender.MALE )
 				borderColor = Color.blue;
-			} else if( sex(node.person) == 2 ) {
+			else if( gender == Gender.FEMALE )
 				borderColor = Color.pink;
-			}
 			setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(borderColor, 2),
 					BorderFactory.createLineBorder(backgroundColor, 10)));
 			addMouseListener( new MouseAdapter() {
@@ -331,9 +340,9 @@ public class Diagram {
 			if( bond.marriageDate != null ) {
 				int y = (int)bond.centerRelY() - MARRIAGE_HEIGHT / 2;
 				g.setColor(new Color(0xFFFFFF));
-				g.fillOval(0, y, (int)bond.width, MARRIAGE_HEIGHT);
+				g.fillOval(0, y, MARRIAGE_WIDTH, MARRIAGE_HEIGHT);
 				g.setColor(new Color(0xAAAAAA));
-				g.drawOval(0, y, (int)bond.width, MARRIAGE_HEIGHT);
+				g.drawOval(0, y, MARRIAGE_WIDTH, MARRIAGE_HEIGHT);
 				g.setColor(Color.black);
 				g.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 				g.drawString(bond.marriageYear(), 0, y + 13);
@@ -354,9 +363,10 @@ public class Diagram {
 			Color backgroundColor = node.acquired ? new Color(0xCCCCCC) : Color.white;
 			setBackground(backgroundColor);
 			Color borderColor = Color.gray;
-			if( sex(node.person) == 1 )
+			Gender gender = Gender.getGender(node.person);
+			if( gender == Gender.MALE )
 				borderColor = Color.blue;
-			else
+			else if( gender == Gender.FEMALE )
 				borderColor = Color.pink;
 			setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(borderColor, 1),
 					BorderFactory.createLineBorder(backgroundColor, 4)));
@@ -372,16 +382,22 @@ public class Diagram {
 	}
 
 	class GraphicLines extends JPanel {
-		Color[] colors = { Color.lightGray, Color.RED, Color.CYAN, Color.MAGENTA, Color.GREEN, Color.PINK, Color.BLACK, Color.YELLOW, Color.BLUE, Color.ORANGE };
-		GraphicLines() {
+		List<Set<Line>> lineGroups;
+		Stroke stroke;
+		Color[] colors = { Color.lightGray, Color.RED, Color.CYAN, Color.MAGENTA, Color.GREEN, Color.PINK, Color.BLACK, Color.YELLOW, Color.BLUE, Color.ORANGE };	
+		GraphicLines(List<Set<Line>> lineGroups, Stroke stroke) {
+			this.lineGroups = lineGroups;
+			this.stroke = stroke;
 			setBounds(shiftX, shiftY, (int)graph.getWidth(), (int)graph.getHeight());
 			setBorder(BorderFactory.createLineBorder(Color.orange, 1));
 			setOpaque(false);
 		}
 		@Override
 		protected void paintComponent(Graphics g) {
+			Graphics2D g2 = (Graphics2D) g;
+			int width = (int)graph.getWidth();
 			int i = 0;
-			for( Set<Line> group : graph.getLines() ) {
+			for( Set<Line> group : lineGroups ) {
 				g.setColor(colors[i % colors.length]);
 				for( Line line : group ) {
 					int x1 = (int)line.x1;
@@ -389,13 +405,19 @@ public class Diagram {
 					int x2 = (int)line.x2;
 					int y2 = (int)line.y2;
 					//g.setColor(Color.lightGray);
+					g2.setStroke(stroke);
 					if( line instanceof CurveLine ) {
-						Graphics2D g2 = (Graphics2D) g;
 						CubicCurve2D cc = new CubicCurve2D.Double();
-						cc.setCurve(x1, y1, x1, y2 , x2, y1, x2, y2);
+						if(orient.isLeftToRight())
+							cc.setCurve(x1, y1, x1, y2 , x2, y1, x2, y2);
+						else
+							cc.setCurve(width - x1, y1, width - x1, y2, width - x2, y1, width - x2, y2);
 						g2.draw(cc);
-					} else {
-						g.drawLine(x1, y1, x2, y2);
+					} else { // Horizontal or vertical line
+						if(orient.isLeftToRight())
+							g.drawLine(x1, y1, x2, y2);
+						else
+							g.drawLine(width - x1, y1, width - x2, y2);
 					}
 				}
 				i++;
@@ -404,31 +426,5 @@ public class Diagram {
 			g.setColor(Color.GRAY);
 			g.drawRect(0, 0, graph.getMaxBitmapWidth(), graph.getMaxBitmapHeight());
 		}
-	}
-
-	/** The sex of a person.
-	 * @param person The dude
-	 * @return int 0 no sex, 1 male, 2 female, 3 undefined, 4 other
-	 */
-	private int sex(Person person) {
-		for (EventFact ef : person.getEventsFacts()) {
-			if (ef.getTag() != null && ef.getTag().equals("SEX")) {
-				if (ef.getValue() == null)
-					return 4; // SEX tag exists but without value
-				else {
-					switch (ef.getValue()) {
-					case "M":
-						return 1;
-					case "F":
-						return 2;
-					case "U":
-						return 3;
-					default:
-						return 4; // other value
-					}
-				}
-			}
-		}
-		return 0; // no SEX tag
 	}
 }
